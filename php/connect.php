67abeea14d18bdc2c8770960a68467c442b80e33
polro <47603822+polro@users.noreply.github.com>
2020-06-04 21:28:23 +0200

    Create connect.php

---
+++ php/connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "servername";
+$username = "username";
+$password = "password";
+$database = "database";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+$conn->set_charset('utf8');
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
